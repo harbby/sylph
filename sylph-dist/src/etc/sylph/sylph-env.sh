0a015f794c584923c2801e3aeee3ebe449410e6e
ideal <125553253@qq.com>
2018-07-26 14:03:15 +0800

    add sylph-env.sh

---
+++ sylph-dist/src/etc/sylph/sylph-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#export JAVA_HOME=/opt/cloudera/parcels/jdk8
+#export FLINK_HOME=/opt/cloudera/parcels/flink
+#export HADOOP_CONF_DIR=/opt/cloudera/parcels/CDH/lib/hadoop/etc/hadoop
+export HADOOP_CONF_DIR=/ideal/hadoop/hadoop/etc/hadoop
+export FLINK_HOME=/ideal/hadoop/flink
