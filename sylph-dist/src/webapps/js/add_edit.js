520bf8c1452f2102241cfa8379ded1c5c6293a58
ideal <125553253@qq.com>
2018-07-26 14:04:51 +0800

    add webapps dir

---
+++ sylph-dist/src/webapps/js/add_edit.js
@@ -0,0 +1,107 @@
+/**
+ * Created by Polar on 2017/12/14.
+ */
+/*获取URL中的参数值*/
+function getUrlParam(paramName) {
+    var arrSource = [];
+    var paramValue = '';
+    //获取url"?"后的查询字符串
+    var search_url = location.search;
+
+    if (search_url.indexOf('?') == 0 && search_url.indexOf('=') > 1) {
+        arrSource = decodeURI(search_url).substr(1).split("&");
+        //遍历键值对
+        for (var i = 0; i < arrSource.length; i++) {
+            if (arrSource[i].indexOf('=') > 0) {
+                if (arrSource[i].split('=')[0].toLowerCase() == paramName.toLowerCase()) {
+                    paramValue = arrSource[i].split("=")[1];
+                    break;
+                }
+            }
+        }
+    }
+    return paramValue;
+}
+
+/*页面加载*/
+$(function () {
+    /*add or edit*/
+    var type = getUrlParam("type");
+    if (type == "add") {
+        $("input,textarea").val('');
+    } else if (type == "edit") {
+        $.ajax({
+            url: "/_sys/job_edit?type=edit&jobId=" + getUrlParam("jobId"),
+            type: "post",
+            dataType: "json",
+            data: {},
+            cache: false,
+            success: function (result) {
+                $("textarea[name=name]").val(result.jobId);
+                $("textarea[name=source]").val(JSON.stringify(result.dag.source, null, 2));
+                $("textarea[name=transform]").val(result.dag.transform);
+                $("textarea[name=sink]").val(result.dag.sink);
+                var files = result.files;
+                for(var i = 0; i < files.length; i++) {
+                    $('#fileList').append(
+                        '<div class="file_row" id="file_' + files[i] + '">' +
+                        '<input type="hidden" name="selectFile" value="' + files[i] + '" />' +
+                        '<i class="fa fa-trash" onclick="deleteFile(this)"></i>' +
+                        '<span class="file_name">'+files[i]+'</span>' +
+                        '</div>');
+                }
+            }
+        });
+    }
+
+    $('#submit').click(function () {
+        var formData = new FormData($('form')[0]);
+        $.ajax({
+            url: '/_sys/job_edit?type=add',
+            type: 'POST',
+            cache: false,
+            data: formData,
+            processData: false,
+            contentType: false
+        }).done(function(data) {
+            var data = JSON.parse(data);
+            if (data.status == "ok") {
+                alert("保存成功");
+                window.location.href = "index.html";
+            } else {
+                alert(data.msg);
+            }
+        }).fail(function(data) {
+            alert(data.msg);
+        });
+    });
+
+    $('input[name=file]').change(function(){
+        $('#fileList').children().remove();
+        var files = $(this).prop('files');
+        for(var i = 0; i < files.length; i++) {
+            $('#fileList').append(
+                '<div class="file_row" id="file_' + files[i].name + '">' +
+                '<input type="hidden" name="selectFile" value="' + files[i].name + '" />' +
+                '<i class="fa fa-trash" onclick="deleteFile(this)"></i>' +
+                '<span class="file_name">'+files[i].name+'</span>' +
+                '</div>');
+        }
+    });
+});
+
+function deleteFile(obj) {
+    $(obj).parent().remove();
+}
+
+var UploadFilesLayer;
+function openUploadFilesLayer() {
+    UploadFilesLayer = layer.open({type: 1,area: ['500px', '360px'],title: '文件上传',shade: 0.6,maxmin: false,
+        anim: 1,content: $('#upload-files')  });
+}
+
+var ConfigSetLayer;
+function openConfigSetLayer() {
+    ConfigSetLayer = layer.open({type: 1,area: ['500px', '360px'],title: '高级配置',shade: 0.6,maxmin: false,
+        anim: 1,content: $('#config-set')  });
+}
---
+++ sylph-dist/src/webapps/js/list.js
@@ -0,0 +1,99 @@
+var url;
+
+$(function () {
+    url = "/_sys/job_manger";
+    var send = {
+        "type": "list", "jobId": ""
+    };
+    $.post(url, JSON.stringify(send), function (data) {
+        data = eval('(' + data + ')');
+        list = data.data;
+        for (var i = 0; i < list.length; i++) {
+            var jobId = list[i].jobId;
+            var create_time = list[i].create_time
+            var yarnId = list[i].yarnId
+            var status = list[i].status;
+            var type = list[i].type;
+            var app_url = list[i].app_url;
+            var button = '';
+            switch (status) {
+                case 'RUNNING':
+                    status = '上线';
+                    button = '<button class="btn btn-primary stop">下线</button>';
+                    break;
+                case 'STOP':
+                    status = '下线';
+                    button = '<button class="btn btn-primary active">上线</button>' + '<button class="btn btn-primary delete">删除</button>' + '<button class="btn btn-primary btn_edit" data-id="' + jobId + '" data-type="'+type+'">编辑</button>';
+                    break;
+                case 'STARTING':
+                    status = '正在启动中';
+                    //button = '<button class="btn btn-primary active">上线</button>' + '<button class="btn btn-primary delete">删除</button>';
+                    break;
+                case 'START_ERROR':
+                    status = '启动失败';
+                    button = '<button class="btn btn-primary stop">下线</button>';
+                    break;
+                default:  //未知状态
+                //status = '未知状态:';
+            }
+            if (yarnId != null && yarnId != '') {
+                yarnId = '<a href="' + app_url + '" target="_blank">' + yarnId + '</a>';
+            }
+            var tmp =
+                '<div class="row">' +
+                '<div class="col-md-2">' + jobId + '</div>' +
+                '<div class="col-md-3">' + yarnId + '</div>' +
+				'<div class="col-md-1">' + type + '</div>' +
+                '<div class="col-md-2">' + create_time + '</div>' +
+                '<div class="col-md-1">' + status + '</div>' +
+                '<div class="col-md-3" jobId="' + jobId + '">' + button + '</div>' +
+                '</div>';
+            $('#rowHead').after(tmp);
+        }
+    });
+
+    $('body').on('click', 'button', function () {
+
+        var send = {
+            "type": "", "jobId": $(this).parent().attr('jobId')
+        };
+        if ($(this).hasClass('active'))   //上线
+        {
+            send.type = 'active'
+        }
+        else if ($(this).hasClass('stop')) {
+            send.type = 'stop'
+        }
+        else if ($(this).hasClass('delete')) {
+            send.type = 'delete'
+        }
+        else if ($(this).hasClass('refresh_all')) {
+            send = {"type": "refresh_all"};
+        } else {
+            return;
+        }
+
+        $.ajax({
+            type: 'post',
+            url: url,
+            async: false,
+            data: JSON.stringify(send),
+            success: function (data) {
+                window.location.reload()
+            }
+        });
+    });
+
+    /*点击编辑跳转页面*/
+    $(document).on("click", ".btn_edit", function () {
+        var id = $(this).attr("data-id");
+        var type = $(this).attr("data-type");
+        if (type == 'flink_sql') {
+            window.location.href = "edit.html?type=edit&jobId=" + id;
+        } else {
+            window.location.href = "etl.html?jobId=" + id;
+        }
+
+    });
+
+});
