d9f83c1509b3d54604ca698a6ff04e3139f9b65a
ideal <125553253@qq.com>
2018-08-24 20:50:45 +0800

    add

--- sylph-spi/src/main/java/ideal/sylph/spi/EtlFlow.java
+++ sylph-spi/src/main/java/ideal/sylph/spi/EtlFlow.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2018 The Sylph Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ideal.sylph.spi;
 
 import ideal.sylph.spi.job.Flow;
@@ -10,10 +25,10 @@ import java.util.List;
  * 用来描述一个job的详细结构
  * 可以转换为多种结构(json 和source-trans-sink 和graph)
  */
-public interface EtlFlow
+public abstract class EtlFlow
         extends Flow
 {
-    List<EdgeInfo> getEdges();
+    public abstract List<EdgeInfo> getEdges();
 
-    List<NodeInfo> getNodes();
+    public abstract List<NodeInfo> getNodes();
 }
