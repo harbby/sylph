6a28611138c8761973f87e63e13db0f9f86a74d2
ideal <125553253@qq.com>
2018-09-11 13:02:13 +0800

    add branch dev: Dev is used for new feature development and will be merged regularly into master

--- sylph-connectors/flink-node-plugin/src/main/scala/ideal/sylph/plugins/flink/sink/MysqlSink.java
+++ sylph-connectors/flink-node-plugin/src/main/scala/ideal/sylph/plugins/flink/sink/MysqlSink.java
@@ -17,6 +17,7 @@ package ideal.sylph.plugins.flink.sink;
 
 import ideal.sylph.annotation.Description;
 import ideal.sylph.annotation.Name;
+import ideal.sylph.etl.PluginConfig;
 import ideal.sylph.etl.Row;
 import ideal.sylph.etl.api.RealTimeSink;
 import org.slf4j.Logger;
@@ -27,9 +28,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Map;
-
-import static java.util.Objects.requireNonNull;
 
 @Name("mysql")
 @Description("this is mysql Sink, if table not execit ze create table")
@@ -38,20 +36,15 @@ public class MysqlSink
 {
     private static final Logger logger = LoggerFactory.getLogger(MysqlSink.class);
 
-    private String url;
-    private String userName;
-    private String password;
+    private final MysqlConfig config;
 
     private Connection connection;
     private PreparedStatement statement;
     private int num = 0;
 
-    @Override
-    public void driverInit(Map<String, Object> optionMap)
+    public MysqlSink(MysqlConfig mysqlConfig)
     {
-        this.url = (String) requireNonNull(optionMap.get("url"), "url is not setting");
-        this.userName = (String) requireNonNull(optionMap.get("userName"), "userName is not setting");
-        this.password = (String) requireNonNull(optionMap.get("password"), "password is not setting");
+        this.config = mysqlConfig;
     }
 
     @Override
@@ -60,7 +53,7 @@ public class MysqlSink
         String sql = "insert into mysql_table_sink values(?,?,?)";
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            this.connection = DriverManager.getConnection(url, userName, password);
+            this.connection = DriverManager.getConnection(config.getJdbcUrl(), config.getUser(), config.getPassword());
             this.statement = connection.prepareStatement(sql);
         }
         catch (SQLException | ClassNotFoundException e) {
@@ -106,6 +99,37 @@ public class MysqlSink
         }
         catch (SQLException e) {
             logger.error("close connection fail", e);
+        }
+    }
+
+    public static class MysqlConfig
+            implements PluginConfig
+    {
+        @Name("jdbc.url")
+        @Description("this is mysql jdbc url")
+        private String jdbcUrl;
+
+        @Name("jdbc.user")
+        @Description("this is mysql userName")
+        private String user;
+
+        @Name("jdbc.password")
+        @Description("this is mysql userName")
+        private String password;
+
+        public String getJdbcUrl()
+        {
+            return jdbcUrl;
+        }
+
+        public String getPassword()
+        {
+            return password;
+        }
+
+        public String getUser()
+        {
+            return user;
         }
     }
 }
