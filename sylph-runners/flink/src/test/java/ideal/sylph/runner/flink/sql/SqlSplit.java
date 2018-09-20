3db9b7f021794104e545e9d53d82b8c5d2702f7e
ideal <125553253@qq.com>
2018-09-20 16:03:15 +0800

    add sqltext split text

---
+++ sylph-runners/flink/src/test/java/ideal/sylph/runner/flink/sql/SqlSplit.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2018 The Sylph Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ideal.sylph.runner.flink.sql;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static ideal.sylph.runner.flink.actuator.FlinkStreamSqlActuator.SqlFlow.SQL_REGEX;
+
+public class SqlSplit
+{
+    @Test
+    public void splitTest1()
+    {
+        String code = "a1;a2;'12;34';\"a4;a8\";10";
+        String[] split = code.split(SQL_REGEX);
+        Assert.assertEquals(split.length, 5);
+        Assert.assertEquals(split, new String[] {"a1", "a2", "'12;34'", "\"a4;a8\"", "10"});
+    }
+}
