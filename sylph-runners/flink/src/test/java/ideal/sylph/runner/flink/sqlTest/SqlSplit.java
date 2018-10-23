901ba08693b2f9e55d1ebbfe24c129e3cae40556
ideal <yezhixinghai@gmail.com>
2018-10-23 21:36:04 +0800

    up test code

--- sylph-runners/flink/src/test/java/ideal/sylph/runner/flink/sqlTest/SqlSplit.java
+++ sylph-runners/flink/src/test/java/ideal/sylph/runner/flink/sqlTest/SqlSplit.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ideal.sylph.runner.flink.sql;
+package ideal.sylph.runner.flink.sqlTest;
 
 import org.junit.Assert;
 import org.junit.Test;
