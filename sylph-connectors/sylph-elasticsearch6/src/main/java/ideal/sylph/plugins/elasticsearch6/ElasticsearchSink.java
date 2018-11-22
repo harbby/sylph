6edd7ab9fc4a3fdbcff565f347a6038bbe4d1665
ideal <yezhixinghai@gmail.com>
2018-11-22 22:52:03 +0800

    add es6

---
+++ sylph-connectors/sylph-elasticsearch6/src/main/java/ideal/sylph/plugins/elasticsearch6/ElasticsearchSink.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (C) 2018 The Sylph Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ideal.sylph.plugins.elasticsearch6;
+
+import ideal.sylph.annotation.Description;
+import ideal.sylph.annotation.Name;
+import ideal.sylph.etl.PluginConfig;
+import ideal.sylph.etl.Row;
+import ideal.sylph.etl.SinkContext;
+import ideal.sylph.etl.api.RealTimeSink;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.action.update.UpdateRequestBuilder;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.apache.flink.shaded.guava18.com.google.common.base.Preconditions.checkState;
+
+@Name("elasticsearch6")
+@Description("this is elasticsearch6 sink plugin")
+public class ElasticsearchSink
+        implements RealTimeSink
+{
+    private final Row.Schema schema;
+    private final ElasticsearchSinkConfig config;
+
+    private TransportClient client;
+    private int idIndex = -1;
+    private final AtomicInteger cnt = new AtomicInteger(0);
+    private BulkRequestBuilder bulkBuilder;
+
+    public ElasticsearchSink(SinkContext context, ElasticsearchSinkConfig config)
+    {
+        schema = context.getSchema();
+        this.config = config;
+        if (!Strings.isNullOrEmpty(config.idField)) {
+            int fieldIndex = schema.getFieldIndex(config.idField);
+            checkState(fieldIndex != -1, config.idField + "不存在, only " + schema.getFields());
+            this.idIndex = fieldIndex;
+        }
+        if (config.update) {
+            checkState(idIndex != -1, "Update mode, `idField` must be set");
+        }
+    }
+
+    @Override
+    public void process(Row value)
+    {
+        Map<String, Object> map = new HashMap<>();
+        for (String fieldName : schema.getFieldNames()) {
+            map.put(fieldName, value.getAs(fieldName));
+        }
+        if (config.update) {  //is update
+            Object id = value.getAs(idIndex);
+            if (id == null) {
+                return;
+            }
+            UpdateRequestBuilder requestBuilder = client.prepareUpdate(config.index, config.type, id.toString());
+            requestBuilder.setDoc(map);
+            bulkBuilder.add(requestBuilder.request());
+        }
+        else {
+            IndexRequestBuilder requestBuilder = client.prepareIndex(config.index, config.type);
+            requestBuilder.setSource(map);
+            bulkBuilder.add(requestBuilder.request());
+        }
+        if (cnt.getAndIncrement() > 100) {
+            client.bulk(bulkBuilder.request()).actionGet();
+            cnt.set(0);
+            bulkBuilder = client.prepareBulk();
+        }
+    }
+
+    @Override
+    public boolean open(long partitionId, long version)
+            throws Exception
+    {
+        String clusterName = config.clusterName;
+        String hosts = config.hosts;
+        Settings settings = Settings.builder().put("cluster.name", clusterName)
+                .put("client.transport.sniff", true).build();
+
+        TransportClient client = new PreBuiltTransportClient(settings);
+        for (String ip : hosts.split(",")) {
+            client.addTransportAddress(
+                    new TransportAddress(InetAddress.getByName(ip.split(":")[0]),
+                            Integer.parseInt(ip.split(":")[1])));
+        }
+        this.client = client;
+        this.bulkBuilder = client.prepareBulk();
+        return true;
+    }
+
+    @Override
+    public void close(Throwable errorOrNull)
+    {
+        try (TransportClient closeClient = client) {
+            if (bulkBuilder != null && closeClient != null) {
+                closeClient.bulk(bulkBuilder.request());
+            }
+        }
+    }
+
+    public static class ElasticsearchSinkConfig
+            extends PluginConfig
+    {
+        @Name("cluster_name")
+        @Description("this is es cluster name")
+        private String clusterName;
+
+        @Name("cluster_hosts")
+        @Description("this is es cluster hosts")
+        private String hosts;
+
+        @Name("es_index")
+        @Description("this is es index")
+        private String index;
+
+        @Name("id_field")
+        @Description("this is es id_field")
+        private String idField;
+
+        @Name("update")
+        @Description("update or insert")
+        private boolean update = false;
+
+        @Name("index_type")
+        @Description("this is es index_type, Do not set")
+        private String type = "default";
+    }
+}
