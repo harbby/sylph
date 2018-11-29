af82f5cd4afdb43e7ad4c95e892579adf3aae3ab
ideal <yezhixinghai@gmail.com>
2018-11-29 12:07:29 +0800

    add windows bat

---
+++ sylph-dist/src/etc/sylph/sylph-env.bat
@@ -0,0 +1,14 @@
+@echo off
+
+@rem set your JAVA_HOME
+@rem set JAVA_HOME=/opt/cloudera/parcels/jdk8
+
+@rem set your HADOOP_CONF_DIR
+@rem set HADOOP_CONF_DIR=/opt/cloudera/parcels/CDH/lib/hadoop/etc/hadoop
+@rem set HADOOP_CONF_DIR=/ideal/hadoop/hadoop/etc/hadoop
+
+@rem set your FLINK_HOME
+@rem set FLINK_HOME=/ideal/hadoop/flink
+
+@rem set your SPARK_HOME
+@rem set SPARK_HOME=/ideal/hadoop/spark
