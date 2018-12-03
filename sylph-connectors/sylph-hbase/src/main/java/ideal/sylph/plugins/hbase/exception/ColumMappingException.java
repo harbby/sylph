c5f97e160bf77b509d4cb1a56b0d5842e3b069ea
wucaifu <wucaifu@zwlbs.com>
2018-12-03 17:29:06 +0800

    commit bhase sink.

---
+++ sylph-connectors/sylph-hbase/src/main/java/ideal/sylph/plugins/hbase/exception/ColumMappingException.java
@@ -0,0 +1,9 @@
+package ideal.sylph.plugins.hbase.exception;
+
+public class ColumMappingException extends RuntimeException{
+
+    public ColumMappingException(String message)
+    {
+        super(message);
+    }
+}
