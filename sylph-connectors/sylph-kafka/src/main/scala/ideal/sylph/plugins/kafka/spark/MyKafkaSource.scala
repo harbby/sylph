68006df365b02a1ad690a7cab012c28d55e3b046
ideal <yezhixinghai@gmail.com>
2019-01-07 17:17:42 +0800

    kafkaSource supports json parser

--- sylph-connectors/sylph-kafka/src/main/scala/ideal/sylph/plugins/kafka/spark/MyKafkaSource.scala
+++ sylph-connectors/sylph-kafka/src/main/scala/ideal/sylph/plugins/kafka/spark/MyKafkaSource.scala
@@ -18,13 +18,13 @@ package ideal.sylph.plugins.kafka.spark
 import ideal.sylph.annotation.{Description, Name, Version}
 import ideal.sylph.etl.PluginConfig
 import ideal.sylph.etl.api.Source
-import org.apache.kafka.clients.consumer.ConsumerRecord
+import ideal.sylph.plugins.kafka.KafkaSourceConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.types._
 import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.streaming.dstream.{DStream, InputDStream}
+import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
@@ -38,60 +38,42 @@ import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 @Description("this spark kafka source inputStream")
 @SerialVersionUID(1L)
 class MyKafkaSource(@transient private val ssc: StreamingContext, private val config: KafkaSourceConfig) extends Source[DStream[Row]] {
-
   /**
     * load stream
     **/
-  private lazy val kafkaStream: InputDStream[ConsumerRecord[String, String]] = {
-    val topics = config.topics
-    val brokers = config.brokers //需要把集群的host 配置到程序所在机器
-    val groupid = config.groupid //消费者的名字
-    val offset = config.offsetMode //
+  private lazy val kafkaStream: DStream[Row] = {
+    val topics = config.getTopics
+    val brokers = config.getBrokers //需要把集群的host 配置到程序所在机器
+    val groupid = config.getGroupid //消费者的名字
+    val offsetMode = config.getOffsetMode
 
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> brokers,
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
       "enable.auto.commit" -> (false: java.lang.Boolean), //不自动提交偏移量
-      //      "session.timeout.ms" -> "30000", //session默认是30秒 超过5秒不提交offect就会报错
+      //      "session.timeout.ms" -> "30000", //session默认是30秒
       //      "heartbeat.interval.ms" -> "5000", //10秒提交一次 心跳周期
       "group.id" -> groupid, //注意不同的流 group.id必须要不同 否则会出现offect commit提交失败的错误
-      "auto.offset.reset" -> offset //latest   earliest
+      "auto.offset.reset" -> offsetMode //latest   earliest
     )
 
-    val topicSets = topics.split(",")
-    KafkaUtils.createDirectStream[String, String](
-      ssc, PreferConsistent, Subscribe[String, String](topicSets, kafkaParams))
-  }
-
-  override def getSource: DStream[Row] = {
     val schema: StructType = StructType(Array(
-      StructField("topic", StringType, nullable = true),
-      StructField("value", StringType, true),
-      StructField("key", StringType, true)
+      StructField("_topic", StringType, nullable = true),
+      StructField("_key", StringType, true),
+      StructField("_message", StringType, true),
+      StructField("_partition", IntegerType, true),
+      StructField("_offset", LongType, true)
     ))
 
-    kafkaStream.map(record =>
-      new GenericRowWithSchema(Array(record.topic(), record.value(), record.key()), schema)
+    val topicSets = topics.split(",")
+    val inputStream = KafkaUtils.createDirectStream[String, String](
+      ssc, PreferConsistent, Subscribe[String, String](topicSets, kafkaParams))
+
+    inputStream.map(record =>
+      new GenericRowWithSchema(Array(record.topic(), record.key(), record.value(), record.partition(), record.offset()), schema)
     ).asInstanceOf[DStream[Row]] //.window(Duration(10 * 1000))
   }
-}
 
-@SerialVersionUID(2L)
-private[this] class KafkaSourceConfig extends PluginConfig {
-  @Name("kafka_topic")
-  @Description("this is kafka topic list")
-  var topics: String = "test1,test2"
-
-  @Name("kafka_broker")
-  @Description("this is kafka broker list")
-  var brokers: String = "localhost:9092"
-
-  @Name("kafka_group_id")
-  @Description("this is kafka_group_id")
-  var groupid: String = "streamEtl1"
-
-  @Name("auto.offset.reset")
-  @Description("this is auto.offset.reset mode")
-  var offsetMode = "latest"
+  override def getSource: DStream[Row] = kafkaStream
 }
