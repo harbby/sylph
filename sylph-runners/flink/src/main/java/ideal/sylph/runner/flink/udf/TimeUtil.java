1d7d60142a11b291c5b7a4b7b7455d19b9fc89ce
ideal <yezhixinghai@gmail.com>
2019-04-09 11:46:14 +0800

    add udf date_format

--- sylph-runners/flink/src/main/java/ideal/sylph/runner/flink/udf/TimeUtil.java
+++ sylph-runners/flink/src/main/java/ideal/sylph/runner/flink/udf/TimeUtil.java
@@ -20,25 +20,60 @@ import ideal.sylph.annotation.Name;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.table.functions.ScalarFunction;
+import org.apache.flink.table.shaded.org.joda.time.DateTime;
+import org.apache.flink.table.shaded.org.joda.time.format.DateTimeFormat;
 
 import java.sql.Timestamp;
+import java.util.Arrays;
 
 public class TimeUtil
 {
+    @Name("date_format")
+    @Description("date_format('2018-01-01 12:00:00', 'yyyy-MM-dd HH:mm:ss')-> TIMESTAMP")
+    public static class DateFormatUDF
+            extends ScalarFunction
+    {
+        @Override
+        public TypeInformation<?> getResultType(Class<?>[] signature)
+        {
+            return Arrays.equals(signature, new Class[] {String.class, String.class}) ? Types.SQL_TIMESTAMP : Types.STRING;
+        }
+
+        public String eval(Timestamp time, String toFormat)
+        {
+            return (new DateTime(time)).toString(toFormat);
+        }
+
+        public String eval(String time, String fromFormat, String toFormat)
+        {
+            return DateTimeFormat.forPattern(fromFormat).parseDateTime(time).toString(toFormat);
+        }
+
+        public Timestamp eval(String time, String fromFormat)
+        {
+            return new Timestamp(DateTimeFormat.forPattern(fromFormat).parseDateTime(time).getMillis());
+        }
+    }
+
     @Name("from_unixtime")
-    @Description("from_unixtime(long)-> TIMESTAMP")
+    @Description("from_unixtime(long)-> TIMESTAMP or from_unixtime(long 13time,varchar to_format)-> varchar")
     public static class FromUnixTime
             extends ScalarFunction
     {
         @Override
         public TypeInformation<?> getResultType(Class<?>[] signature)
         {
-            return Types.SQL_TIMESTAMP;
+            return signature.length == 2 ? Types.STRING : Types.SQL_TIMESTAMP;
         }
 
         public Timestamp eval(long time)
         {
             return new Timestamp(time);
+        }
+
+        public String eval(long time, String format)
+        {
+            return (new DateTime(time)).toString(format);
         }
     }
 }
